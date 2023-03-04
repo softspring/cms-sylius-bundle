5975a20fb0dcba8f6accceede8d35b674041376c
Javi H. Gil <javihgil@softspring.es>
2023-03-04 01:33:40 +0100

    Fix translation paths loading

--- src/DependencyInjection/Compiler/AddTranslationsPathsPass.php
+++ src/DependencyInjection/Compiler/AddTranslationsPathsPass.php
@@ -19,19 +19,14 @@ class AddTranslationsPathsPass implements CompilerPassInterface
         $collection = $definition->getArgument(0);
 
         foreach ($container->getParameter('sfs_cms.collections') as $collectionPath) {
-            // add modules translations if exists
-            $modulesPath = $container->getParameter('kernel.project_dir') . '/' . trim($collectionPath, '/') . '/modules';
-            if (!is_dir($modulesPath)) {
-                continue;
-            }
-
-            foreach ((new Finder())->directories()->in("$modulesPath/*")->name('translations') as $transDirectory) {
-                foreach ((new Finder())->in($transDirectory->getRealPath())->files() as $file) {
-                    $fileNameParts = explode('.', $file->getBasename());
-                    $format = array_pop($fileNameParts);
-                    $locale = array_pop($fileNameParts);
-                    $domain = implode('.', $fileNameParts);
-                    $collection[] = $file->getRealPath();
+            foreach (['module', 'block', 'content', 'layout', 'menu', 'site'] as $item) {
+                $path = $container->getParameter('kernel.project_dir').'/'.trim($collectionPath, '/')."/{$item}s";
+                if (is_dir($path)) {
+                    foreach ((new Finder())->directories()->in("$path/*")->name('translations') as $transDirectory) {
+                        foreach ((new Finder())->in($transDirectory->getRealPath())->files() as $file) {
+                            $collection[] = $file->getRealPath();
+                        }
+                    }
                 }
             }
         }
