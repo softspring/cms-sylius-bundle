4e87d6f79418c649c9ea95bffd175409bdecbbb7
Eva <ebelmonte82@gmail.com>
2024-03-14 14:02:59 +0100

    Separate bootstrap and vanilla scripts (#44)
    * Separate bootstrap and vanilla scripts
    
    * Rename bootstrap to content-edit
    
    * Add confirmation to unpublish version

---
+++ assets/scripts/admin-cms-content-edit.js
@@ -0,0 +1,23 @@
+/* JS Vanilla dependencies */
+import '@softspring/cms-bundle/scripts/admin/content-edit/config-errors';
+import '@softspring/cms-bundle/scripts/admin/content-edit/content-enter-inserts-module';
+import '@softspring/cms-bundle/scripts/admin/content-edit/content-tab-contenteditable';
+import '@softspring/cms-bundle/scripts/admin/content-edit/site-filter-modules';
+import '@softspring/cms-bundle/scripts/admin/content-edit/locale-filter-modules';
+import '@softspring/cms-bundle/scripts/admin/content-edit/filter-preview';
+import '@softspring/cms-bundle/scripts/admin/content-edit/preview-background-color';
+import '@softspring/cms-bundle/scripts/admin/content-edit/preview-block';
+import '@softspring/cms-bundle/scripts/admin/content-edit/preview-class';
+import '@softspring/cms-bundle/scripts/admin/content-edit/preview-collection-node-class';
+import '@softspring/cms-bundle/scripts/admin/content-edit/preview-fill';
+import '@softspring/cms-bundle/scripts/admin/content-edit/preview-id';
+import '@softspring/cms-bundle/scripts/admin/content-edit/preview-media-modal';
+import '@softspring/cms-bundle/scripts/admin/content-edit/preview-tag-type';
+import '@softspring/cms-bundle/scripts/admin/content-edit/preview-toggle';
+
+/* With bootstrap dependencies */
+import '@softspring/cms-bundle/scripts/admin/content-edit/wysiwyg';
+import '@softspring/cms-bundle/scripts/admin/content-edit/modules';
+import '@softspring/cms-bundle/scripts/admin/content-edit/contenteditable';
+import '@softspring/cms-bundle/scripts/admin/content-edit/contenteditable-focus';
+import '@softspring/cms-bundle/scripts/admin/content-edit/preview-media-choice';
---
+++ assets/scripts/admin-cms.js
@@ -0,0 +1,10 @@
+import '@softspring/cms-bundle/scripts/tools';
+
+import '@softspring/cms-bundle/scripts/admin/routes-forms';
+import '@softspring/cms-bundle/scripts/admin/form-collections';
+import '@softspring/cms-bundle/scripts/admin/fields-visibility';
+import '@softspring/cms-bundle/scripts/admin/locales-widgets';
+
+import '@softspring/cms-bundle/scripts/types/block-type';
+import '@softspring/cms-bundle/scripts/types/color-type';
+import '@softspring/cms-bundle/scripts/types/symfony-route-type';
