b0f12e08303d5437a4b92ebd47600a6ea1904dcf
ebpe <ebelmonte82@gmail.com>
2024-03-14 12:23:47 +0100

    Rename bootstrap to content-edit

--- assets/scripts/admin-cms-bootstrap.js
+++
@@ -1,6 +0,0 @@
-/* With bootstrap dependencies */
-import '@softspring/cms-bundle/scripts/admin/content-edit/wysiwyg';
-import '@softspring/cms-bundle/scripts/admin/content-edit/modules';
-import '@softspring/cms-bundle/scripts/admin/content-edit/contenteditable';
-import '@softspring/cms-bundle/scripts/admin/content-edit/contenteditable-focus';
-import '@softspring/cms-bundle/scripts/admin/content-edit/preview-media-choice';
--- assets/scripts/admin-cms.js
+++ assets/scripts/admin-cms.js
@@ -1,21 +1,5 @@
 import '@softspring/cms-bundle/scripts/tools';
 
-import '@softspring/cms-bundle/scripts/admin/content-edit/config-errors';
-import '@softspring/cms-bundle/scripts/admin/content-edit/content-enter-inserts-module';
-import '@softspring/cms-bundle/scripts/admin/content-edit/content-tab-contenteditable';
-import '@softspring/cms-bundle/scripts/admin/content-edit/site-filter-modules';
-import '@softspring/cms-bundle/scripts/admin/content-edit/locale-filter-modules';
-import '@softspring/cms-bundle/scripts/admin/content-edit/filter-preview';
-import '@softspring/cms-bundle/scripts/admin/content-edit/preview-background-color';
-import '@softspring/cms-bundle/scripts/admin/content-edit/preview-block';
-import '@softspring/cms-bundle/scripts/admin/content-edit/preview-class';
-import '@softspring/cms-bundle/scripts/admin/content-edit/preview-collection-node-class';
-import '@softspring/cms-bundle/scripts/admin/content-edit/preview-fill';
-import '@softspring/cms-bundle/scripts/admin/content-edit/preview-id';
-import '@softspring/cms-bundle/scripts/admin/content-edit/preview-media-modal';
-import '@softspring/cms-bundle/scripts/admin/content-edit/preview-tag-type';
-import '@softspring/cms-bundle/scripts/admin/content-edit/preview-toggle';
-
 import '@softspring/cms-bundle/scripts/admin/routes-forms';
 import '@softspring/cms-bundle/scripts/admin/form-collections';
 import '@softspring/cms-bundle/scripts/admin/fields-visibility';
