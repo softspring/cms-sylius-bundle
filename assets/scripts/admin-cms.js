9271d1d22878bd7fcfe2b0f78d41e665a97d0ac0
Eva <ebelmonte82@gmail.com>
2025-07-07 12:01:03 +0200

    Add json-diff-patch (#107)

--- assets/scripts/admin-cms.js
+++ assets/scripts/admin-cms.js
@@ -4,6 +4,7 @@ import '@softspring/collection-form-type/scripts/collection-form-type';
 import '@softspring/cms-bundle/scripts/admin/routes-forms';
 import '@softspring/cms-bundle/scripts/admin/fields-visibility';
 import '@softspring/cms-bundle/scripts/admin/locales-widgets';
+import '@softspring/cms-bundle/scripts/admin/versions-diff';
 
 import '@softspring/cms-bundle/scripts/types/block-type';
 import '@softspring/cms-bundle/scripts/types/color-type';
